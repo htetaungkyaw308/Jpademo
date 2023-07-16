b485f3494fe8435e2b7f2d78f002b404230e78c1
DESKTOP-N9SEVUS\Terry <aungpyaepaing2020@gmail.com>
2023-07-16 14:24:31 +0630

    first commit

---
+++ src/test/java/com/jwd39/jpa/JPA/Demo/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwd39.jpa.JPA.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
